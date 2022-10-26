a26093e20b20779089da28bd9abc2cfdf2d1c076
FelipeDasr <felipe.heiker07@gmail.com>
2022-10-26 15:02:34 -0400

    feat: unfinished codes added

---
+++ listaMC.cpp
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "listaMC.h"
+
+void inserirM(marca *&noMarca, int codigo, float nota) {
+  marca *noMarcaAux = noMarca;
+  printf("Chamou");
+  marca *novoNo = (marca *)calloc(1, sizeof(marca));
+  novoNo->codigo = codigo;
+  novoNo->nota = nota;
+
+  if (noMarca == NULL) {
+    noMarca = novoNo;
+  } else {
+    while (noMarcaAux->prox != NULL) {
+      noMarcaAux = noMarcaAux->prox;
+    }
+    noMarcaAux->prox = novoNo;
+  }
+}
+
+float buscaMarca(marca *noMarca, int codigo) {}
+
+void inserirC(carro *&noCarro, char *nome, int ano, int codigoMarca) {}
+
+void listagem(marca *noMarca, carro *noCarro) {}
+
+void removerM(marca *&noMarca, carro *noCarro, int) {}
+
+void desalocaMarca(marca *&noMarca) {}
+
+void desalocaCarro(carro *&noCarro) {}
---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include "listaMC.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/*funcao main*/
+int main() {
+
+  int op;
+  marca *noMarca;
+  carro *noCarro;
+
+  do {
+    printf("\n1 - Cadastrar uma marca");
+    printf("\n2 - Cadastrar um carro");
+    printf("\n3 - Listagem de carros");
+    printf("\n4 - Exclusao de uma marca");
+    printf("\n5 - Sair");
+    printf("\nOpcao: ");
+    scanf("%d", &op);
+
+    if (op == 1) {
+      int codigo;
+      float nota;
+
+      scanf("%d %f", &codigo, &nota);
+      inserirM(noMarca, codigo, nota);
+    } else if (op == 2) {
+
+      int codigoMarca;
+      int ano;
+      char nome[40];
+
+      scanf("%d %s %d", &codigoMarca, nome, &ano);
+      inserirC(noCarro, nome, ano, codigoMarca);
+
+    } else if (op == 3) {
+
+    } else if (op == 4) {
+    }
+
+  } while (op != 5);
+
+  // desalocar listas
+
+  return 0;
+}
